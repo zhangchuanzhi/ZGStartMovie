245db428cb590227247085abb2c252b2f194927b
zhangzhi0903 <18310127735@163.com>
2018-03-23 15:13:42 +0800

    Initial Commit

---
+++ ZGStartMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGStartMovie
+//
+//  Created by offcn_zcz32036 on 2018/3/23.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
