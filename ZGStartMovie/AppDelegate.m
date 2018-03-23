339a546202ee50bb5edfe2fb707ecc0fc4ae8ffe
zhangzhi0903 <18310127735@163.com>
2018-03-23 15:28:08 +0800

    启动加播放器播放一段视频

--- ZGStartMovie/AppDelegate.m
+++ ZGStartMovie/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "WSMovieController.h"
+#import "RootTabBarController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +17,29 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    self.window.backgroundColor = [UIColor whiteColor];
+    NSString *versionCache = [[NSUserDefaults standardUserDefaults] objectForKey:@"VersionCache"];//本地缓存的版本号  第一次启动的时候本地是没有缓存版本号的。
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];//当前应用版本号
+
+    if (![versionCache isEqualToString:version]) //如果本地缓存的版本号和当前应用版本号不一样，则是第一次启动（更新版本也算第一次启动）
+    {
+        WSMovieController *wsCtrl = [[WSMovieController alloc]init];
+        wsCtrl.movieURL = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"qidong"ofType:@"mp4"]];//选择本地的视屏
+        self.window.rootViewController = wsCtrl;
+
+        //设置上下面这句话，将当前版本缓存到本地，下次对比一样，就不走启动视屏了。
+        //也可以将这句话放在WSMovieController.m的进入应用方法里面
+        //为了让每次都可以看到启动视屏，这句话先注释掉
+        //[[NSUserDefaults standardUserDefaults] setObject:version forKey:@"VersionCache"];
+
+    }else{
+        //不是首次启动
+        RootTabBarController *rootTabCtrl = [[RootTabBarController alloc]init];
+        self.window.rootViewController = rootTabCtrl;
+    }
+    [self.window makeKeyAndVisible];  
     return YES;
 }
 
